0987914266417d87c544d68a88ee7bf8bfcb7ba6
Kyle Kurth <kkurth@boise.cs.colostate.edu>
2015-02-12 15:48:53 -0700

    added Profile.php

---
+++ Profile.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="description" content="Project 1 profile page">
+	<meta name="keywords" content="social networking, CS, web design">
+	<meta name="author" content="Kyle Kurth">
+	<title>Profile</title>
+</head>
+	
+<body>
+	<div class="header">
+	
+	</div>
+
+	<div class="contents">
+	<img id="profile picture">
+	</div>
+	
+	<div class="footer">
+	
+	</div>
+</body>
+
+
+</html>
